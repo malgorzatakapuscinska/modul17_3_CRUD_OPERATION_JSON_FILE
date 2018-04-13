fef6dad394471e02af8127832a9474d7560c30b8
Malgorzata Kapuscinska <m_kapuscinska1@o2.pl>
2018-04-13 14:48:35 +0200

    Ready task

--- server.js
+++ server.js
@@ -1,59 +1,44 @@
 var express = require('express');
-
 var fs = require('fs');
-
 var bodyParser = require('body-parser');
 
-
-
 var app = express();
-
 var stringifyFile;
-
-
 
 app.use(bodyParser.json());
 
-
-
-app.get('/getNote', function(req, res) {
-
-    fs.readFile('./test.json', 'utf8', function(err, data) {
-
-        if (err) throw err;
-
-        stringifyFile = data;
-
-        res.send(data);
-
-    });
-
+fs.readFile('./test.json', 'utf-8', function(err, data){
+	if (err) throw err;
+	stringifyFile=data;
+	console.log(stringifyFile);
 });
 
 
+app.get('/getNote', function(req, res){
+	fs.readFile('./test.json', 'utf-8', function(err, data){
+	if (err) throw err;
+	stringifyFile=data;
+	res.send(data);
+	});
+});
 
 app.post('/updateNote/:note', function(req, res) {
 
-    parsedFile = JSON.parse(stringifyFile);
-
-    parsedFile.note = req.params.note;
-
-    stringifyFile = JSON.stringify(parsedFile);
-
-    fs.writeFile('./test.json', stringifyFile, function(err) {
-
-        if (err) throw err;
-
-        console.log('file updated');
-
-    });
-
+		parsedFile = JSON.parse(stringifyFile);
+		parsedFile.note = req.params.note;
+		stringifyFile = JSON.stringify(parsedFile);
+		
+	fs.writeFile('./test.json', stringifyFile, function(err) {
+	    if (err) throw err;
+	    console.log('file updated');
+	});
+	res.send('Zawartość test.json po dodaniu notatki: ' + stringifyFile);
 });
 
 
+var server= app.listen(3000);
 
-var server = app.listen(3000, function() {
-
-    console.log('Aplikacja nasłuchuje na http://localhost:3000');
-
+app.use(function(req, res, next){
+	res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego czego żądasz :-(')
 });
+
